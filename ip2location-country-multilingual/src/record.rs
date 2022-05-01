a2f41b0dc3f80e972b976e439f5e1e1e0ac574fd
vkill <vkill.net@gmail.com>
2022-05-01 13:51:39 +0800

    Update ip2location-country-multilingual

--- ip2location-country-multilingual/src/record.rs
+++ ip2location-country-multilingual/src/record.rs
@@ -1,12 +1,14 @@
+use country_code::{iso3166_1::alpha_3::CountryCode as CountryCode3, CountryCode};
+use language_code::LanguageTag;
 use serde::Deserialize;
 
 //
 #[derive(Deserialize, Debug, Clone)]
 pub struct Record {
-    pub lang: Box<str>,
+    pub lang: LanguageTag,
     pub lang_name: Box<str>,
-    pub country_alpha2_code: Box<str>,
-    pub country_alpha3_code: Box<str>,
+    pub country_alpha2_code: CountryCode,
+    pub country_alpha3_code: CountryCode3,
     pub country_numeric_code: Box<str>,
     pub country_name: Box<str>,
 }
--- ip2location-country-multilingual/tests/static.rs
+++ ip2location-country-multilingual/tests/static.rs
@@ -1,12 +1,28 @@
 #![cfg(feature = "once_cell")]
 
+use country_code::CountryCode;
 use ip2location_country_multilingual::RECORDS;
+use language_code::{LanguageCode, LanguageTag};
 
 #[test]
 fn test_static() {
+    //
     let record = RECORDS
         .iter()
-        .find(|x| x.lang == "EN".into() && x.country_alpha2_code == "US".into())
+        .find(|x| {
+            x.lang == LanguageTag::new(LanguageCode::en, None)
+                && x.country_alpha2_code == CountryCode::US
+        })
+        .unwrap();
+    println!("{:?}", record);
+
+    //
+    let record = RECORDS
+        .iter()
+        .find(|x| {
+            x.lang == LanguageTag::new(LanguageCode::zh, Some(CountryCode::CN))
+                && x.country_alpha2_code == CountryCode::US
+        })
         .unwrap();
     println!("{:?}", record);
 }
